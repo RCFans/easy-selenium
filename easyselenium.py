9214614665f21bc78e2ef2f24c0a5e88488d7094
Justina Chen <rcfans@163.com>
2015-08-27 21:46:04 +0800

    remove reduplicated import

--- easyselenium.py
+++ easyselenium.py
@@ -4,11 +4,10 @@
 # <codecell>
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
